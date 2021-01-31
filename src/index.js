96f9f236872e126b656fb31444e0d875796dfc5d
Khushi-Shukla <tusharika.shukla05@gmail.com>
2021-01-31 11:35:47 +0530

    Configuring react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render(   //rendering the view to the DOM It is rendered in index.html
   <React.StrictMode>
     <App />
   </React.StrictMode>,
