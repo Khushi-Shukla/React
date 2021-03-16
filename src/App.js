c3409a8697af682ebcc21c0441b8c2bb06155aaa
unknown <tusharika.shukla05@gmail.com>
2021-03-16 20:09:59 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 //import {Navbar, NavbarBrand} from 'reactstrap';
 // import  Menu  from './components/MenuComponent';
 import Main from './components/MainComponent';
+import './App.css'
 
 class App extends Component {
 
