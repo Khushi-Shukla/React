bf8f8f62d2fdb2e6ba90435cefbd05eeab13af98
unknown <tusharika.shukla05@gmail.com>
2021-03-21 20:05:58 +0530

    Intro to redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-//import logo from './logo.svg';
 import React, { Component } from 'react';
-//import {Navbar, NavbarBrand} from 'reactstrap';
-// import  Menu  from './components/MenuComponent';
 import Main from './components/MainComponent';
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore'
 
+const store= ConfigureStore();
 class App extends Component {
 
+  
   render() {
     return (
-      <BrowserRouter>
-      <div>
-        <Main />
-      </div>
-      </BrowserRouter>
+      <Provider store={store} >
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
